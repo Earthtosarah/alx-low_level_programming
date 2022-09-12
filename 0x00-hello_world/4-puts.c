a28a514eb4769cbd365b4e4bb6f8bccc8de8cf16
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-12 00:07:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - Entry point
+ * block of code
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
-
