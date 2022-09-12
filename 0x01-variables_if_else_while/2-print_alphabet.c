86f83764bed69b502584fb911c6bfc9bf2550ebe
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-12 15:12:10 -0700

    letters of the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description - prints letters of the alphabet in  lowercase
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+		putchar('\n');
+	return (0);
+}
