ef787946f3fe9c39b6b633eab7f709e361b6a97c
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-14 19:34:57 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table -prints tables
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	a = 0;
+	
+	while (a <= 9)
+	{
+		b = 0;
+
+		while (b <= 9)
+		{
+			c = a * b;
+			_putchar(',');
+			_putchar(' ');
+			if (c <= 9)
+			{
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+
