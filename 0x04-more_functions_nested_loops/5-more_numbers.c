9f6c142512b41c1acf58ba6969e411d0b1a3e100
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-16 00:07:56 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14.
+ * Return: 0;
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			_putchar('\n');
+		}
+		i++;
+	}
