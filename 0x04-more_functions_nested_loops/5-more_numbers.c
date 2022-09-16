4aa45cf532da2c3fca5619363b223d2e3a574981
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-16 00:10:11 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14.
- * Return: 0;
+ * Return: 0
  */
 
 void more_numbers(void)
@@ -24,3 +24,4 @@ void more_numbers(void)
 		}
 		i++;
 	}
+}
