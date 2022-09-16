dfa8e6c65863f764665c9f3ac228691c1dfe5597
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-16 13:25:12 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,24 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
-			printf(" ");
+		}
+		if (i != 100)
+		{
+			printf(" ")
 		}
 	}
+	printf("\n")
 	return (0);
 }
