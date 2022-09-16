1e31b9221ad82919995e4a35c401af5e4cc536d6
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-16 01:15:07 -0700

    pring line)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; (i > n && n > 0); i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
