d7065ee30b8a03fb9c26d23307d193bcb5df8032
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-16 02:19:48 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check for a digit and prints a diagonsl line
+ * @n : number of \\ to be printed
+ * Return:void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n && n > 0)
+		{
+			j = 0;
+			while (j > i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+}
