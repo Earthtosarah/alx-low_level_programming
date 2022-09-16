a65c400449fc8e53e8206dec2488328f1cfb1c67
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-16 14:51:18 -0700

    1

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - displays a right triangular pattern
+ * @size: an integer
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	/* row number */
+	int j;
+	/* number of spaces before '#' symbol */
+	int k;
+	/* # */
+
+	if (size <= 0)
+	{
+		_putchar("\n");
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(" ");
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar("#");
+			}
+			_putchar("\n");
+		}
+	}
+}
