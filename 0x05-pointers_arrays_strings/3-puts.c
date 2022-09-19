90dccef2e9a5c2a41003ec9132123d8982869758
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-19 15:18:20 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * *@str: A pointer to an int
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	char *c;
+	int n;
+
+	c = str;
+	n = 0;
+
+	while (c[n])
+	{
+		_putchar(c[n]);
+		n++;
+	}
+	_putchar('\n')
+}
