eb68884ffe493419bcbed7c4789504f0fe3d21f2
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-19 19:58:49 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse of a string
+ * @s: pointer
+ * Return: Nothing
+ */
+void print_rev(char *s);
+{
+	int n = 0;
+	int len = 0;
+
+	while (s[n] != 0)
+	{
+		len += 1;
+		n++;
+	}
+	n = n - 1;
+	while (n >= 0)
+	{
+		_putchar(s[n]);
+		n--;
+	}
+	_putchar('\n')
+
+}
