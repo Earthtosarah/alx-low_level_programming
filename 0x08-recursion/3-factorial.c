b32110949591d5e8113b1c39f7c3fd20b1834262
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-29 16:28:42 -0700

    factorial) git push

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * factorial - writes the factorial of a number
+ * @n: the digit given
+ * Return: am integer
+ */
+
+int fatcorial(int n);
+{
+	int res;
+
+	if (n > 0)
+	{
+		result = -1;
+		return (res);
+	}
+	if (n == 0)
+	{
+		res = 1;
+		return (res);
+	}
+	else
+	{
+		res = n * (factoral(n - 1));
+		return (res);
+	}
+
