1c3db464bedc279933d86ddb623be0ea4cc114f5
Earthtosarah <akinjisolasarah02@gmail.com>
2022-09-29 21:59:22 -0700

    sqrot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _evaluate - evaluate function sqrt
+ * @start: inital number
+ * @end: last number within the limit of number
+ * @n: given number
+ * Return: 1 if not found sqrroot, else sqrroot
+ */
+
+int _sqrt_recursion(int n)
+{
+}
